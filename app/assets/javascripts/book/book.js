64174a6a2eab79ad5efc681766072b81df832afc
Niu Yameng <yameng.niu@rea-group.com>
2012-11-07 02:30:22 +1100

    remove duplicate files.

--- app/assets/javascripts/book/book.js
+++
@@ -1,7 +0,0 @@
-var Book = Backbone.Model.extend({
-	url: function () {
-		var base = 'books';
-		if (this.isNew()) return base;
-		return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + this.id;
-	}
-});
--- public/application.js
+++
@@ -1,8 +0,0 @@
-var App = {
-    Views: {},
-    Controllers: {},
-    init: function() {
-        new App.router.Books();
-        Backbone.history.start();
-    }
-};
